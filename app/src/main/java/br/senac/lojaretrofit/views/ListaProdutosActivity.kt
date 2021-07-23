27c63aa1c5c3089ef39849e0b857a7b2a2ac34a4
Igor Kenzo <36746677+IgorKenzo@users.noreply.github.com>
2021-07-23 17:20:04 -0300

    a

--- app/src/main/java/br/senac/lojaretrofit/views/ListaProdutosActivity.kt
+++ app/src/main/java/br/senac/lojaretrofit/views/ListaProdutosActivity.kt
@@ -1,12 +1,17 @@
 package br.senac.lojaretrofit.views
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import br.senac.lojaretrofit.R
 import br.senac.lojaretrofit.databinding.ActivityListaProdutosBinding
 import br.senac.lojaretrofit.databinding.CardItemBinding
 import br.senac.lojaretrofit.model.Produto
 import br.senac.lojaretrofit.services.ProdutoService
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
 import okhttp3.OkHttpClient
@@ -24,6 +29,14 @@ class ListaProdutosActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityListaProdutosBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.shimmer.visibility = View.INVISIBLE
+        binding.scrollView.visibility = View.INVISIBLE
+        binding.shimmer.stopShimmer()
+
+        binding.swipeRefresh.setOnRefreshListener {
+            atualizarProdutos()
+        }
     }
 
     override fun onResume() {
@@ -31,6 +44,7 @@ class ListaProdutosActivity : AppCompatActivity() {
         atualizarProdutos()
     }
     fun atualizarProdutos() {
+        binding.scrollView.visibility = View.INVISIBLE
         val http = OkHttpClient.Builder()
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
@@ -50,6 +64,11 @@ class ListaProdutosActivity : AppCompatActivity() {
 
         val callback = object : Callback<List<Produto>> {
             override fun onResponse(call: Call<List<Produto>>, response: Response<List<Produto>>) {
+                //binding.progressBar.visibility = View.INVISIBLE
+                binding.shimmer.visibility = View.INVISIBLE
+                binding.shimmer.stopShimmer()
+                binding.swipeRefresh.isRefreshing = false
+                binding.scrollView.visibility = View.VISIBLE
                 if(response.isSuccessful) {
                     val listaProdutos = response.body()
                     attTela(listaProdutos)
@@ -66,6 +85,11 @@ class ListaProdutosActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<List<Produto>>, t: Throwable) {
+                //binding.progressBar.visibility = View.INVISIBLE
+                binding.shimmer.visibility = View.INVISIBLE
+                binding.shimmer.stopShimmer()
+                binding.swipeRefresh.isRefreshing = false
+                binding.scrollView.visibility = View.VISIBLE
                 Snackbar.make(
                     binding.container,
                     "Não deu certo",
@@ -77,7 +101,9 @@ class ListaProdutosActivity : AppCompatActivity() {
         }
 
         call.enqueue(callback)
-
+        //binding.progressBar.visibility = View.VISIBLE
+        binding.shimmer.visibility = View.VISIBLE
+        binding.shimmer.startShimmer()
     }
 
     fun attTela(listaProdutos : List<Produto>?) {
@@ -88,8 +114,24 @@ class ListaProdutosActivity : AppCompatActivity() {
             cardBiding.textNome.text = it.nomeProduto
             cardBiding.textPreco.text = it.precProduto.toString()
 
+            val shimmer = Shimmer.ColorHighlightBuilder()
+                .setDuration(1000)
+                .setBaseAlpha(0.5f)
+                .setBaseColor(getColor(R.color.placeholder_grey))
+                .setHighlightColor(Color.WHITE)
+                .setHighlightAlpha(0.75f)
+                .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+                .setAutoStart(true)
+                .build()
+
+            val shimmerDrawable = ShimmerDrawable()
+            shimmerDrawable.setShimmer(shimmer)
+
             Picasso.get()
-                .load("https://oficinacordova.azurewebsites.net/android/rest/produto/image/${it.idProduto}")
+                //.load("https://oficinacordova.azurewebsites.net/android/rest/produto/image/${it.idProduto}")
+                .load("https://drive.google.com/file/d/13mJ_iSOOXIwLGLm07zHurw23Db5Ak9rb/view?usp=sharing")
+                .placeholder(shimmerDrawable)
+                .error(R.drawable.no_image)
                 .into(cardBiding.imagem)
 
             binding.container.addView(cardBiding.root)
